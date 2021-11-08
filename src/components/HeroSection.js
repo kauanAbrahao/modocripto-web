e96de4b86977c1426abfa27393b956133b9ee5d9
kauanabrahao <kauan.martins@fatec.sp.gov.br>
2021-11-08 00:07:17 -0300

    Ajuste plano de fundo

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -4,19 +4,21 @@ import './HeroSection.css'
 
 function HeroSection() {
     return (
-      <div className='hero-container'>
-        <h1>Dados Internacionais de Criptomoedas</h1>
-        <p>Atualize sua aplicação em tempo real</p>
-        <div className='hero-btns'>
-          <Button
-            className='btns'
-            buttonStyle='btn--outline'
-            buttonSize='btn--large'
-          >
-            Entenda a API
-          </Button>
-        </div>
-        <div className='hero-img'></div>
+        <div className='hero-master'>
+            <img className="hero-img" src="https://analyticsinsight.b-cdn.net/wp-content/uploads/2021/06/Profitable-Cryptocurrencies-1024x576.jpeg" alt='Crypto'/>
+                <div className="hero-text">
+                    <h1>Dados Internacionais de Criptomoedas</h1>
+                    <p>Atualize sua aplicação em tempo real</p>
+                    <div className='hero-btns'>
+                    <Button
+                        className='btns'
+                        buttonStyle='btn--outline'
+                        buttonSize='btn--large'
+                    >
+                        Entenda a API
+                    </Button>
+                </div>
+            </div>
       </div>
     );
   }
