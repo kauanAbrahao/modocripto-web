78a7a8fff7ddb771ec43845247ca728eb58edb50
kauanabrahao <kauan.martins@fatec.sp.gov.br>
2021-11-13 12:36:48 -0300

    Página servicos finalizada

--- src/components/ServicesCardSection.js
+++ src/components/ServicesCardSection.js
@@ -11,22 +11,37 @@ function ServicesCardSection() {
                     <div class="col-lg-4">
                         <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
                             <div class="features-icons-icon d-flex"><i class="bi-window m-auto text-primary"></i></div>
-                            <h3>Fully Responsive</h3>
-                            <p class="lead mb-0">This theme will look great on any device, no matter the size!</p>
+                                <h3 className='title'>Valor</h3>
+                                <h5 className="sub-title"><i>"current_price"</i></h5>
+                                <p class="lead mb-0">Preço atual da criptomoeda. Calculado através da média entre as diferentes bolsas de valores nacionais e internacionais.</p>
+                                <h5 className='sub-title'><i>"avg_price"</i></h5>
+                                <p class="lead mb-0">Preço médio da criptomoeda. Dado exclusivamente histórico. </p>
+                                <h5 className='sub-title'><span><i>"high_24h" & "low_24h" </i> </span> </h5>
+                                <p class="lead mb-0">Maior e menor valor da criptomoeda para determinado dia, respectivamente </p>
+                       
                         </div>
                     </div>
                     <div class="col-lg-4">
                         <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
                             <div class="features-icons-icon d-flex"><i class="bi-layers m-auto text-primary"></i></div>
-                            <h3>Bootstrap 5 Ready</h3>
-                            <p class="lead mb-0">Featuring the latest build of the new Bootstrap 5 framework!</p>
+                                <h3 className='title'>Volume</h3>
+                                <h5 className="sub-title"><i>"total_volume"</i></h5>
+                                <p class="lead mb-0">Volume total de criptomoedas circulando no mercado.</p>
+                                <h5 className="sub-title"><i>"total_volume_24h"</i></h5>
+                                <p class="lead mb-0">Volume total de criptomoedas em determinada data.</p>
+                            
                         </div>
                     </div>
                     <div class="col-lg-4">
                         <div class="features-icons-item mx-auto mb-0 mb-lg-3">
                             <div class="features-icons-icon d-flex"><i class="bi-terminal m-auto text-primary"></i></div>
-                            <h3>Easy to Use</h3>
-                            <p class="lead mb-0">Ready to use with your own content, or customize the source files!</p>
+                            <h3 className='title'>Capitalização de Mercado</h3>
+                                <h5 className="sub-title"><i>"mkt_cap_rank"</i></h5>
+                                <p class="lead mb-0">Rank de Capitalização de Mercado; ordena as criptomoedas pelo rank atual.</p>
+                                <h5 className='sub-title'><i>"mkt_cap"</i></h5>
+                                <p class="lead mb-0">Capitalização de mercado atual para cada criptomoeda. </p>
+                                <h5 className='sub-title'><span><i>"avg_mkt_cap" </i> </span> </h5>
+                                <p class="lead mb-0">Capitalização média da criptomoeda. Dado exclusivamente histórico.</p>
                         </div>
                     </div>
                 </div>
