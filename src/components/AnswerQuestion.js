fe8c8ee3e6a3b42cf1c2c423fcb0142cb1e26426
gperseguini <gperseguini@outlook.com>
2022-05-29 16:25:21 -0300

    Modelo FAQ adicionado

--- src/components/AnswerQuestion.js
+++ src/components/AnswerQuestion.js
@@ -2,7 +2,42 @@ import React from 'react'
 import './AnswerQuestion.css'
 
 function AnswerQuestion() {
-    return  (<h1>FAQ</h1>);
+    return  (
+        <section classNameName="py-5 border-bottom" id="features">
+            <h1 className='myTitle'>FAQ</h1>
+            <div className="container px-5 my-5">
+                <h5 className="h4 fw-bolder">1. Quais são os principais dados oferecidos pela plataforma?</h5>
+                <p>Os principais dados disponibilizados pela plataforma são: Valor, Volume e Capitalização de mercado. 
+                Você pode encontrar mais informações no link a seguir: <br></br>
+                <a className="text-decoration-none" href="http://localhost:3000/servicos" target='_blank' rel="noreferrer">
+                            Clique aqui
+                </a>
+                </p>
+                <h5 className="h4 fw-bolder">2. Quais são os principais dados oferecidos pela plataforma?</h5>
+                <p>Os principais dados disponibilizados pela plataforma são: Valor, Volume e Capitalização de mercado. 
+                Você pode encontrar mais informações no link a seguir: <br></br>
+                <a className="text-decoration-none" href="http://localhost:3000/servicos" target='_blank' rel="noreferrer">
+                            Clique aqui
+                </a>
+                </p>
+                <h5 className="h4 fw-bolder">3. Quais são os principais dados oferecidos pela plataforma?</h5>
+                <p>Os principais dados disponibilizados pela plataforma são: Valor, Volume e Capitalização de mercado. 
+                Você pode encontrar mais informações no link a seguir: <br></br>
+                <a className="text-decoration-none" href="http://localhost:3000/servicos" target='_blank' rel="noreferrer">
+                            Clique aqui
+                </a>
+                </p>
+                <h5 className="h4 fw-bolder">4. Quais são os principais dados oferecidos pela plataforma?</h5>
+                <p>Os principais dados disponibilizados pela plataforma são: Valor, Volume e Capitalização de mercado. 
+                Você pode encontrar mais informações no link a seguir: <br></br>
+                <a className="text-decoration-none" href="http://localhost:3000/servicos" target='_blank' rel="noreferrer">
+                            Clique aqui
+                </a>
+                </p>
+            </div>
+        </section>
+    
+    );
 }
 
 export default AnswerQuestion
