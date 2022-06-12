7e0e896bad56664f83957693b35387d75c3304ce
gperseguini <gperseguini@outlook.com>
2022-06-12 19:00:16 -0300

    finalizacao contato

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,10 +2,25 @@ import React from 'react'
 import './Contact.css'
 
 function Contact() {
-    return  (
+    return  (  
     <section classNameName="py-5 border-bottom" id="features">
         <h1 className='myTitle'>Formulário de Contato</h1>
-        <div className="container px-5 my-5">
+        <div className="forms">
+        <form>
+            <div >
+                <label> Nome <input className='def' type="text" name="name"/> </label>
+            </div>
+            <div >
+                <label> Assunto <input className='def' type="text" name="subject"/> </label>
+            </div>
+            <div >
+                <label> E-mail <input  className='def' type="text" name="mail"/> </label>
+            </div>
+        <div>
+           <label> Mensagem: </label><textarea name="message" ></textarea> 
+        </div>
+        <button type="submit">Enviar</button>
+        </form>
             
         </div>
     </section>    
